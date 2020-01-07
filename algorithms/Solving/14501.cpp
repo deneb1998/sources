f65997d957b9ef89bb8bd4e55544e912ac2d5776
심우철-싸지방 <simm4256@naver.com>
2020-01-07 22:19:34 +0900

    dd

--- algorithms/Solving/14501.cpp
+++ algorithms/Solving/14501.cpp
@@ -10,8 +10,26 @@ https://www.acmicpc.net/problem/14501
 #define min(a,b) ((a)>(b)?(b):(a))
 using namespace std;
 
-int n, t[15], p[15];
+int n, t[15], p[15], ans = -1;
+
+void func(int x, int sum) {
+	if (x == n) {
+		ans = max(ans, sum);
+		return;
+	}
+	if (x > n)
+		return;
+	func(x + t[x], sum + p[x]);
+	func(x + 1, sum);
+}
 
 int main() {
-	
+	ios::sync_with_stdio(false);
+
+	int i;
+	cin >> n;
+	for (i = 0; i < n; i++)
+		scanf("%d %d", t + i, p + i);
+	func(0, 0);
+	cout << ans;
 }
