7e21a56c6d15d80012d1f1dbb72f635cea42ffc5
심우철-싸지방 <simm4256@naver.com>
2020-01-07 22:02:30 +0900

    ㅇㅇ

---
+++ algorithms/Solving/14501.cpp
@@ -0,0 +1,17 @@
+/*
+14501 Επ»η
+https://www.acmicpc.net/problem/14501
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)>(b)?(b):(a))
+using namespace std;
+
+
+
+int main() {
+
+}
