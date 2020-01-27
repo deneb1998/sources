a1d24eee66f19f059d8c88faf9f8740146f3cfe0
Simwoocheol <simm4256@naver.com>
2020-01-28 00:00:10 +0900

    dd

--- algorithms/Solving/Solving/Solving.cpp
+++ algorithms/Solving/Solving/Solving.cpp
@@ -1,44 +1,50 @@
 /*
-이항 계수
-https://www.acmicpc.net/problem/3651
+이항 계수 4
+https://www.acmicpc.net/problem/11402
 */
 
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
+#include <vector>
+typedef long long ll;
 #define max(a,b) ((a)>(b)?(a):(b))
 #define min(a,b) ((a)>(b)?(b):(a))
 using namespace std;
 
-int c[21][21];
-int a, b;
+ll n, k, m, ans = 1;
+vector<ll> v1, v2;
+ll dp[2005][2005];
 
-int calc(int n, int k) {
-	if (c[n][k]) return c[n][k];
-	if (n - k < k) return c[n][k] = calc(n, n - k);
-	if (k == 1) return c[n][k] = n;
-	if (k == 0) return c[n][k] = 1;
-	return c[n][k] = calc(n - 1, k - 1) + calc(n - 1, k);
+ll calc(ll x, ll y) {
+	if (dp[x][y])
+		return dp[x][y];
+	if (!y || x == y)
+		return 1;
+	if (y > x - y)
+		return dp[x][y] = calc(x, x - y);
+	return dp[x][y] = calc(x - 1, y - 1) + calc(x - 1, y);
 }
 
 int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	cin >> a >> b;
-	int i, j;
-	for(i=1;i<21;i++)
-		for (j = 1; j <= i; j++) {
-			if (!c[i][j]) {
-				c[i][j] = calc(i, j);
-			}
-		}
-	while (1) {
-		cin >> a;
-		for (i = 1; i < 21; i++)
-			for (j = 1; j <= i; j++) {
-				if (c[i][j] == a) {
-					cout << i << ' ' << j << '\n';
-				}
-			}
+
+	cin >> n >> k >> m;
+	while (n) {
+		v1.push_back(n % m);
+		n /= m;
 	}
+	while (k) {
+		v2.push_back(k % m);
+		k /= m;
+	}
+	int i, len = v1.size(), len2 = v2.size();
+	for (i = 0; i < len; i++) {
+		if (len2 - 1 < i)
+			continue;
+		ans *= calc(v1[i], v2[i]);
+		ans %= m;
+	}
+	cout << ans;
 }
