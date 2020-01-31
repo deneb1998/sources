bab1de8f3eee9cc341dcaee9717dc030910802e4
simm4256 <simm4256@naver.com>
2020-02-01 00:00:55 +0900

    Update Solving.cpp

--- algorithms/Solving/Solving/Solving.cpp
+++ algorithms/Solving/Solving/Solving.cpp
@@ -1,6 +1,6 @@
 /*
-이항 계수 4
-https://www.acmicpc.net/problem/11402
+XOR
+https://www.acmicpc.net/problem/12844
 */
 
 #include <iostream>
@@ -12,39 +12,75 @@ typedef long long ll;
 #define min(a,b) ((a)>(b)?(b):(a))
 using namespace std;
 
-ll n, k, m, ans = 1;
-vector<ll> v1, v2;
-ll dp[2005][2005];
+int n, m, t, a, b, c, len = 0;
+int seg_tree[1050000];
 
-ll calc(ll x, ll y) {
-	if (dp[x][y])
-		return dp[x][y];
-	if (!y || x == y)
-		return 1;
-	if (y > x - y)
-		return dp[x][y] = calc(x, x - y);
-	return dp[x][y] = calc(x - 1, y - 1) + calc(x - 1, y);
+int seg_sum(int s, int e, int x) {
+	if (s > e)
+		return 0;
+	if (s == e) {
+		if (s >= a && e <= b)
+			return seg_tree[x];
+		return 0;
+	}
+	if (s >= a && e <= b)
+		return seg_tree[x];
+	if (s > b || e < a)
+		return 0;
+	return seg_sum(s, (s+e)/2, x * 2) ^ seg_sum((s+e) / 2 + 1, e, x * 2 + 1);
+}
+
+void seg_change(int s, int e, int x) {
+	if (s > e)
+		return;
+	if (s == e) {
+		if (s >= a && e <= b)
+			seg_tree[x] ^= c;
+		return;
+	}
+	if (s >= a && e <= b) {
+		seg_tree[x] ^= ((e - s + 1) % 2 ? c : 0);
+		seg_change(s, (s + e) / 2, x * 2);
+		seg_change((s + e) / 2 + 1, e, x * 2 + 1);
+		return;
+	}
+	if (s > b || e < a) 
+		return;
+	if (a >= s && b <= e) {
+		seg_tree[x] ^= ((b - a + 1) % 2 ? c : 0);
+		seg_change(s, (s + e) / 2, x * 2);
+		seg_change((s + e) / 2 + 1, e, x * 2 + 1);
+		return;
+	}
+	seg_tree[x] ^= ((e - a + 1) % 2 ? c : 0);
+	seg_change(s, (s + e) / 2, x * 2);
+	seg_change((s + e) / 2 + 1, e, x * 2 + 1);
 }
 
 int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	cin >> n >> k >> m;
-	while (n) {
-		v1.push_back(n % m);
-		n /= m;
+	int i, j, k;
+	cin >> n;
+	for (i = 1; i < n; i *= 2);
+	for (k = i; k < n + i; k++) {
+		cin >> seg_tree[k];
+		for (j = k / 2; j >= 1; j /= 2) {
+			seg_tree[j] ^= seg_tree[k];
+		}
 	}
-	while (k) {
-		v2.push_back(k % m);
-		k /= m;
+	cin >> m;
+	while (m--) {
+		cin >> t >> a >> b;
+		a++, b++;
+		if (a > b) swap(a, b);
+		if (t == 2) {
+			cout << seg_sum(1, i, 1) << '\n';
+		}
+		else {
+			cin >> c;
+			seg_change(1, i, 1);
+		}
 	}
-	int i, len = v1.size(), len2 = v2.size();
-	for (i = 0; i < len; i++) {
-		if (len2 - 1 < i)
-			continue;
-		ans *= calc(v1[i], v2[i]);
-		ans %= m;
-	}
-	cout << ans;
 }
