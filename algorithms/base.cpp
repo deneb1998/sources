e1e6b5fc4aec68ab0b0dc796d0ad0d23f6b7bae7
simm4256 <simm4256@naver.com>
2020-02-08 14:56:36 +0900

    Create base.cpp

---
+++ algorithms/base.cpp
@@ -0,0 +1,20 @@
+/*
+문제 이름
+문제 링크
+*/
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+typedef long long ll;
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+}
