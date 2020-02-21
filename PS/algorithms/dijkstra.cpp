9131f457924d0724d0cf2776ea3b574fb0dc84ef
simm4256 <simm4256@naver.com>
2020-02-21 23:39:38 +0900

    Update dijkstra.cpp

--- PS/algorithms/dijkstra.cpp
+++ PS/algorithms/dijkstra.cpp
@@ -3,6 +3,25 @@
 O(|E||log|V|)
 */
 
-void dijkstra(){
-
+void dijkstra() {
+	memset(dist, -1, sizeof(dist));
+	priority_queue<pair<int, int> > pq;
+	dist[s] = 0;
+	pq.emplace(0, s);
+	int cnt = 0;
+	while (!pq.empty()) {
+		int x = pq.top().second, val = pq.top().first;
+		pq.pop();
+		if (dist[x] != -1)
+			continue;
+		dist[x] = val;
+		if (++cnt == n || x == e)
+			break;
+		for (auto i : edge[x]) {
+			if (dist[i.second] != -1)
+				continue;
+			pq.emplace(i.first + val, i.second);
+		}
+	}
+	cout << dist[e];
 }
