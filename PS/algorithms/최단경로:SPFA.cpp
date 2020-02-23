0ba3954150c4e36c2c2172144985e2bfba21b11c
simm4256 <simm4256@naver.com>
2020-02-23 23:55:03 +0900

    Update and rename 최단경로 : SPFA.cpp to 최단경로:SPFA.cpp

--- PS/algorithms/최단경로:SPFA.cpp
+++ PS/algorithms/최단경로:SPFA.cpp
@@ -1,11 +1,12 @@
 bool spfa() {
+	int s = startNumber;
 	bool isInQ[510]; memset(isInQ, 0, sizeof(isInQ));
 	fill(dist, dist + 509, INF);
-	dist[1] = 0;
+	dist[s] = 0;
 	queue<int> q;
-	q.push(1);
-	isInQ[1] = true;
-	cnt[1] = 1;
+	q.push(s);
+	isInQ[s] = true;
+	cnt[s] = 1;
 	while (!q.empty()) {
 		int x = q.front();
 		q.pop();
@@ -15,7 +16,7 @@ bool spfa() {
 				continue;
 			dist[i.to] = dist[x] + i.v;
 			if (!isInQ[i.to]) {
-				if (++cnt[i.to] > n)
+				if (++cnt[i.to] > n) //주의점 : if 밖에서 실행하면 안됨
 					return false;
 				q.push(i.to);
 				isInQ[i.to] = true;
