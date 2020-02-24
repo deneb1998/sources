1f0e84d29d898e9fa2ac464c618cb67a04276870
simm4256 <simm4256@naver.com>
2020-02-24 23:58:50 +0900

    Update Solving.cpp

--- PS/Solving.cpp
+++ PS/Solving.cpp
@@ -1,6 +1,6 @@
 /*
-웜홀
-https://www.acmicpc.net/problem/1865
+토렌트
+https://www.acmicpc.net/problem/9577
 */
 
 #include <iostream>
@@ -11,56 +11,75 @@ https://www.acmicpc.net/problem/1865
 #define INF ((1<<31) - 1)
 using namespace std;
 
-int tc, n, m, w, a, b, c, cnt[510], dist[510];
-struct node {
-	int to, v;
-};
-vector<node> edge[6010];
+int t, n, m, a, b, c, x, level[205], ans, cap[205][205], flow[205][205], can[105][105], maxT;
 
-bool spfa() {
-	bool isInQ[510]; memset(isInQ, 0, sizeof(isInQ));
-	fill(dist, dist + 509, INF);
-	dist[1] = 0;
+vector<int> edge[205];
+
+bool leveling() {
+	memset(level, -1, sizeof(level));
+	level[0] = 1;
 	queue<int> q;
-	q.push(1);
-	isInQ[1] = true;
-	cnt[1] = 1;
+	q.push(0);
 	while (!q.empty()) {
-		int x = q.front();
+		int now = q.front();
 		q.pop();
-		isInQ[x] = false;
-		for (auto i : edge[x]) {
-			if (dist[x] + i.v >= dist[i.to])
+		for (auto i : edge[now]) {
+			if (level[i] != -1 || cap[now][i] <= flow[now][i])
 				continue;
-			dist[i.to] = dist[x] + i.v;
-			if (!isInQ[i.to]) {
-				if (++cnt[i.to] > n)
-					return false;
-				q.push(i.to);
-				isInQ[i.to] = true;
-			}
+			level[i] = level[now] + 1;
+			q.push(i);
 		}
 	}
-	return true;
+	return (level[maxT + n + 1] != -1);
+}
+
+bool dinic(int maxT) {
+	memset(flow, 0, sizeof(flow));
+	while (leveling()) {
+
+	}
 }
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL);
-	cin >> tc;
-	while (tc--) {
-		memset(cnt, 0, sizeof(cnt));
-		for (int i = 0; i < 6010; i++)
-			edge[i].clear();
-		cin >> n >> m >> w;
+	//ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> t;
+	while (t--) {
+		memset(cap, 0, sizeof(cap));
+		memset(can, -1, sizeof(can));
+
+		cin >> n >> m;
+		maxT = -1;
+		int i, j;
 		while (m--) {
 			cin >> a >> b >> c;
-			edge[a].push_back(node{ b, c });
-			edge[b].push_back(node{ a, c });
+			if (a == b) continue;
+			maxT = max(maxT, b);
+			while (c--) {
+				cin >> x;
+				for (i = a + 1; i <= b; i++)
+					can[i][x] = 1;
+			}
 		}
-		while (w--) {
-			cin >> a >> b >> c;
-			edge[a].push_back(node{ b, c });
+		for (i = 1; i <= maxT; i++) {
+			edge[0].push_back(i);
+			cap[0][i] = 1;
+			for (j = 1; j <= n; j++) {
+				if (can[i][j] != 1)
+					continue;
+				edge[i].push_back(maxT + j);
+				cap[i][maxT + j] = 1;
+			}
 		}
-		cout << (spfa() ? "NO" : "YES");
+		for (i = maxT + 1; i <= maxT + n; i++) {
+			edge[i].push_back(maxT + n + 1);
+			cap[i][maxT + n + 1] = 1;
+		}
+		if (!dinic(maxT)) {
+			cout << -1 << '\n';
+			continue;
+		}
+		for (i = n; i < maxT; i++)
+			if (dinic(i)) break;
+		cout << i << '\n';
 	}
 }
