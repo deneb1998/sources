4fb156f21555495e4940b3cafb1f65a391fe79e9
simm4256 <simm4256@naver.com>
2020-03-25 23:11:30 +0900

    Update base.cpp

--- PS/base.cpp
+++ PS/base.cpp
@@ -1,13 +1,19 @@
 /*
-Hashing
-https://www.acmicpc.net/problem/15829
+듣보잡
+https://www.acmicpc.net/problem/1764
 */
+
 #include <bits/stdc++.h>
-#define INF ((1<<31) - 1)
+#define PI 3.141592
+#define INF ((1<<31)-1)
 using namespace std;
-typedef unsigned int uint;
 typedef long long ll;
 typedef unsigned long long ull;
+typedef unsigned int uint;
+
+int dx[] = { -1,1,0,0 }, dy[] = { 0,0,-1,1 };
+
+/*              PS start              */
 
 
 
